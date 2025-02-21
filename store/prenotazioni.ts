72c69441f2db0207146d521940b38cf9f49c1b91
Airali06 <airali.cld@gmail.com>
2025-02-21 13:47:07 +0100

    insertPrenotazione

--- store/prenotazioni.ts
+++ store/prenotazioni.ts
@@ -32,21 +32,35 @@ export const usePrenotazioni = defineStore("prenotazioni-store", {
 
         this.prenotazioni = [...response[0]] as Array<Prenotazione>;
 
-        /*
-        for (const prenotazione of response) {
-          // Verifica che ogni elemento abbia la forma giusta (facoltativo)
-          let temp = {
-            id_prenotazione: prenotazione.id_prenotazione * 1,
-            id_utente: prenotazione.id_utente * 1,
-            id_postazione: prenotazione.id_postazione * 1,
-            n_modifiche: prenotazione.n_modifiche * 1,
-            data: new Date(prenotazione.data),
-            flag: prenotazione.flag,
-          } as Prenotazione;
-          this.prenotazioni.push({ ...temp });
-          console.log(prenotazione);
-          this.prenotazioni.push(prenotazione as Prenotazione);
-        }*/
+        console.log(this.prenotazioni);
+      } catch (e) {
+        console.log("errore" + e);
+      }
+    },
+
+    async nuovaPrenotazione(data: number, id_postazione: number) {
+      const authStore = useAuth();
+
+      try {
+        const response = (await $fetch(
+          authStore.address + "insertPrenotazioni.php",
+          {
+            //composizione dell messaggio di request
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json", //specifica tipologia di dato inviata
+            },
+            body: JSON.stringify({
+              //trasforma un oggetto in JSON (json obbligatorio per mandare dati al server)
+              id_utente: authStore.utente.id_utente,
+              id_postazione: id_postazione,
+              data: data,
+            }),
+          }
+        )) as any;
+
+        console.log(response[0]);
+        this.getPrenotazioni();
 
         console.log(this.prenotazioni);
       } catch (e) {
